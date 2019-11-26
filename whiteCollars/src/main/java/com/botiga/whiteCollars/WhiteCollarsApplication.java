72ca204744b24ea7d41cc9261a8682a0c2807356
yomandrake <yomandrake@gmail.com>
2019-11-26 10:25:05 +0100

    First Commit. Create new Maven SpringBoot Project
    -Create project with Spring Boot Initializr
    
    Project: Simple Spring Boot project for a collars store named White Collars.

---
+++ whiteCollars/src/main/java/com/botiga/whiteCollars/WhiteCollarsApplication.java
@@ -0,0 +1,13 @@
+package com.botiga.whiteCollars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteCollarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteCollarsApplication.class, args);
+	}
+
+}
---
+++ whiteCollars/src/test/java/com/botiga/whiteCollars/WhiteCollarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botiga.whiteCollars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteCollarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
